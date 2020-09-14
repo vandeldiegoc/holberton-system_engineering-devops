4e4bde424f1fa8b827f93b78a8c3653e12667a84
diego <vandeldiegoc@gmail.com>
2020-09-14 17:41:17 -0500

    0, 1[add]

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/urs/bin/python3
+"""create scrip"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    n = requests.get('https://jsonplaceholder.typicode.com/users/' + argv[1])
+    name = n.json().get("name")
+    link_todos = 'https://jsonplaceholder.typicode.com/todos'
+    task = requests.get(link_todos, params={'userId': argv[1]})
+    tasks = task.json()
+    count = 0
+    total = 0
+    list_all = []
+
+    for x in tasks:
+        if x['completed'] is True:
+            list_all.append(x['title'])
+            count = count + 1
+        total += 1
+    print("Employee {} is done with tasks({}/{}):".format(name, count, total))
+    for y in list_all:
+        print("\t{}".format(y))
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/urs/bin/python3
+"""create scrip"""
+import requests
+from sys import argv
+import csv
+
+if __name__ == "__main__":
+
+    n = requests.get('https://jsonplaceholder.typicode.com/users/' + argv[1])
+    username = n.json().get('username')
+    link_todos = 'https://jsonplaceholder.typicode.com/todos'
+    task = requests.get(link_todos, params={'userId': argv[1]})
+    tasks = task.json()
+    list_all = []
+    list_status = []
+    for x in tasks:
+        list_all.append(x['title'])
+        list_status.append(x['completed'])
+    with open("{}.csv".format(argv[1]), "w") as csvfile:
+            writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+            for y in range(len(list_status)):
+                writer.writerow([argv[1], username, list_status[y],
+                                list_all[y]])
