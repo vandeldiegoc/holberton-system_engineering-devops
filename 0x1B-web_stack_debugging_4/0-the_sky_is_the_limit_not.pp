3f6eaafd9db8af947ac8bddd26041f81ee703ff0
Diego <vandeldiegoc@gmail.com>
2020-10-08 13:15:06 -0500

    0[add]

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,4 +3,4 @@ exec { 'upperlimit' :
   command =>  'sed -i "s/15/10000/g" /etc/default/nginx;service nginx restart',
   path    =>  '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   onlyif  =>  'test -e /etc/default/nginx',
-}}
+}
